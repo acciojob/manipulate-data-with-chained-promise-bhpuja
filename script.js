5ea4d96a7502c90e45867d54fc24f01352525e91
bhpuja <vpuja0421@gmail.com>
2025-02-04 13:04:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+function getNumbers() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]);
+        }, 3000);
+    });
+}
+
+function filterEvenNumbers(numbers) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const evens = numbers.filter(num => num % 2 === 0);
+            document.getElementById("output").innerText = `Filtered Evens: ${evens}`;
+            resolve(evens);
+        }, 1000);
+    });
+}
+
+function multiplyByTwo(numbers) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const doubled = numbers.map(num => num * 2);
+            document.getElementById("output").innerText = `Doubled Evens: ${doubled}`;
+            resolve(doubled);
+        }, 2000);
+    });
+}
+
+getNumbers()
+    .then(filterEvenNumbers)
+    .then(multiplyByTwo)
+    .catch(error => console.error("Error:", error));
